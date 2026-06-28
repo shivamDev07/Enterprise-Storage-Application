69c6f6daa158b08377d7ee12718e7e88be021c47
Shivam <shivamsharma8025ss@gmail.com>
2026-06-28 22:22:22 +0530

    add initial configuration setup

---
+++ src/test/java/com/example/EnterpriseStorageApplication/EnterpriseStorageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.EnterpriseStorageApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnterpriseStorageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
